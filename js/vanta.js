e1497b144f338a2c68c9b4fc79cd5bf42c3eebb0
tipalsyahhh <tipalsyah123@gmail.com>
2024-03-26 11:53:26 +0700

    quis-smp

---
+++ js/vanta.js
@@ -0,0 +1,20 @@
+VANTA.BIRDS({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0xffffff,
+    color1: 0xb7a4f,
+    color2: 0x1ea070,   
+    birdSize: 1.40,
+    wingSpan: 18.00,
+    speedLimit: 8.00,
+    separation: 27.00,
+    alignment: 17.00,
+    cohesion: 26.00,
+    quantity: 3.00
+})
